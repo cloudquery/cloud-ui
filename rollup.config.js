20dae40afd2ff84fc7afb25fa89a16c36efbccb3
Erez Rokah <erezrokah@users.noreply.github.com>
2025-09-30 11:26:34 +0100

    chore: Fix build (#16)

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,23 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import { terser } from "rollup-plugin-terser";
-import mergeFilesPlugin from './merge-files-plugin';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import terser from '@rollup/plugin-terser';
+import mergeFilesPlugin from './merge-files-plugin.js';
 
 export default {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: [
     {
-      dir: "dist",
-      format: "cjs",
+      dir: 'dist',
+      format: 'cjs',
       sourcemap: true,
-      exports: "named",
+      exports: 'named',
       entryFileNames: '[name].cjs.js',
     },
     {
-      dir: "dist",
-      format: "esm",
+      dir: 'dist',
+      format: 'esm',
       sourcemap: true,
       entryFileNames: '[name].esm.js',
     },
@@ -29,20 +29,21 @@ export default {
     /^@mui\/system.*/,
     'react',
     'react-dom',
+    'tslib',
   ],
   plugins: [
     peerDepsExternal(),
     resolve(),
     commonjs(),
-    typescript({ tsconfig: "./tsconfig.json", outDir: 'dist' }),
+    typescript({ tsconfig: './tsconfig.json', outDir: 'dist' }),
     terser({
       format: {
         comments: /webpackIgnore:/,
       },
     }),
     mergeFilesPlugin({
-      sourceFile: './lib/theme/types.d.ts', 
-      targetFile: 'index.d.ts'
+      sourceFile: './lib/theme/types.d.ts',
+      targetFile: 'index.d.ts',
     }),
   ],
 };
